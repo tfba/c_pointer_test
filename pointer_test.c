ff69bb60f9f269184f877438f53d2a2465981408
trustfarm <trustfarm.info@gmail.com>
2019-11-06 15:26:22 +0900

    C pointer test1 and mem layouts

---
+++ pointer_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+extern void hexdump(void *mem, unsigned int len);
+
+int func1(void)
+{
+	printf("func1 \n\n");
+}
+
+
+char vowels[][5] = {
+    {'A', 'E', 'I', 'O', 'U'},
+    {'a', 'e', 'i', 'o', 'u'},
+    {'1', '2', '3', '4', '5'},
+    {'t', 'r', 'u', 's', 't'},
+    {'f', 'a', 'r', 'm', '!'}
+};
+
+int a34[3][4] = {  
+   {0, 1, 2, 3} ,   /*  initializers for row indexed by 0 */
+   {4, 5, 6, 7} ,   /*  initializers for row indexed by 1 */
+   {8, 9, 10, 11}   /*  initializers for row indexed by 2 */
+};
+
+
+char *hellostr = "Hello world\n";
+
+int a12[3][4] = {0,1,2,3,4,5,6,7,8,9,10,11};
+char hellostrarr[] = "array_Hello world\n";
+
+
+int  main()
+{
+	printf("Hello world ~ \nhellostr = \"%s\" \n hellostrarr = \"%s\"\n", hellostr, hellostrarr);
+
+    hexdump(vowels, 500);
+
+	func1();
+	return 1;
+}
+
